7b2c5d90cf8d43fea022a4967d3e67bed6edd027
Olivia <126028295+ozp2@users.noreply.github.com>
2025-07-11 15:43:04 -0400

    Initial Commit

---
+++ habit-trackerTests/habit_trackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  habit_trackerTests.swift
+//  habit-trackerTests
+//
+//  Created by Olivia on 7/11/25.
+//
+
+import Testing
+@testable import habit_tracker
+
+struct habit_trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
