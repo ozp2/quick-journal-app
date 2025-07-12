fe34591861164d580651cb743889ff864ada7c0f
Olivia <126028295+ozp2@users.noreply.github.com>
2025-07-11 21:33:45 -0400

    dark mode

--- my-thoughts/Color+Theme.swift
+++ my-thoughts/Color+Theme.swift
@@ -1,23 +1,57 @@
 import SwiftUI
+import AppKit
 
 extension Color {
-    static let surface = Color(red: 0xF8/255, green: 0xFA/255, blue: 0xF8/255)
-    static let surfaceContainer = Color(red: 0xEF/255, green: 0xF1/255, blue: 0xEF/255)
-    static let lightGrey = Color(red: 0xf1/255, green: 0xec/255, blue: 0xe9/255)
-    static let tag = Color(red: 0xed/255, green: 0xe2/255, blue: 0xdc/255)
-    static let hoverColor = Color(red: 0xe5/255, green: 0xde/255, blue: 0xd9/255)
-    static let medium = Color(red: 0xc7/255, green: 0xc1/255, blue: 0xbd/255)
-    static let mediumDark = Color(red: 0x8d/255, green: 0x86/255, blue: 0x81/255)
-    static let dark = Color(red: 0x22/255, green: 0x18/255, blue: 0x1c/255)
-    static let mainText = Color(red: 0x29/255, green: 0x1d/255, blue: 0x15/255)
-    static let secondaryText = Color(red: 0x67/255, green: 0x5a/255, blue: 0x51/255)
+    // Background colors that adapt to light/dark mode
+    static let surface = Color(
+        light: Color(red: 0xF8/255, green: 0xFA/255, blue: 0xF8/255),
+        dark: Color(red: 0x1C/255, green: 0x1C/255, blue: 0x1E/255)
+    )
+    
+    static let surfaceContainer = Color(
+        light: Color(red: 0xEF/255, green: 0xF1/255, blue: 0xEF/255),
+        dark: Color(red: 0x2C/255, green: 0x2C/255, blue: 0x2E/255)
+    )
+    
+    // Text colors that adapt to light/dark mode
+    static let mainText = Color(
+        light: Color(red: 0x29/255, green: 0x1d/255, blue: 0x15/255),
+        dark: Color(red: 0xF2/255, green: 0xF2/255, blue: 0xF7/255)
+    )
+    
+    // Accent color
     static let accent = Color(red: 60/255, green: 72/255, blue: 172/255)
-    static let pop = Color(red: 0xcc/255, green: 0x3f/255, blue: 0x0c/255)
-    static let darkAccent = Color(red: 0x2c/255, green: 0x39/255, blue: 0x23/255)
-    static let lightRed = Color(red: 0xcc/255, green: 0x3f/255, blue: 0x0c/255)
-    static let lightGreen = Color(red: 0x60/255, green: 0x78/255, blue: 0x4f/255)
-    static let errorRed = Color(red: 0xcc/255, green: 0x3f/255, blue: 0x0c/255)
-    static let success = Color(red: 0x55/255, green: 0x8B/255, blue: 0x6E/255) // #558B6E
-    static let successLight = Color(red: 0xD6/255, green: 0xEA/255, blue: 0xDF/255) // #D6EADF
+    
+    // Status colors
+    static let lightRed = Color(
+        light: Color(red: 0xcc/255, green: 0x3f/255, blue: 0x0c/255),
+        dark: Color(red: 0xFF/255, green: 0x69/255, blue: 0x58/255)
+    )
+    
+    static let errorRed = Color(
+        light: Color(red: 0xcc/255, green: 0x3f/255, blue: 0x0c/255),
+        dark: Color(red: 0xFF/255, green: 0x69/255, blue: 0x58/255)
+    )
+    
+    static let success = Color(
+        light: Color(red: 0x55/255, green: 0x8B/255, blue: 0x6E/255),
+        dark: Color(red: 0x30/255, green: 0xD1/255, blue: 0x58/255)
+    )
+    
+    static let successLight = Color(
+        light: Color(red: 0xD6/255, green: 0xEA/255, blue: 0xDF/255),
+        dark: Color(red: 0x1A/255, green: 0x3A/255, blue: 0x2E/255)
+    )
+}
 
+extension Color {
+    init(light: Color, dark: Color) {
+        self.init(NSColor(name: nil) { appearance in
+            if appearance.bestMatch(from: [.darkAqua, .aqua]) == .darkAqua {
+                return NSColor(dark)
+            } else {
+                return NSColor(light)
+            }
+        })
+    }
 } 
